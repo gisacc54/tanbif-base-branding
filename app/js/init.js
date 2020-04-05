bde76e1964e614963075b82e4a6b5e97f397d7dd
Gisacc54 <giftisacc54@gmail.com>
2020-04-05 12:47:42 +0300

    bug fixing

--- app/js/init.js
+++ app/js/init.js
@@ -2,10 +2,6 @@ require('./settings.js').default;
 require('./index-auth.js');
 require('./i18next-config.js');
 require('./mante.js');
-require('./environment.js');
-require('./base/custom.js');
-require('./base/vendor.js');
-require('./base/pages/home.js');
 
 // require('./stats.js');
 
